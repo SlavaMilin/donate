0fa17f5aea347c6004aced22768e99b17b9f1a3b
SlavaMilin <slava.milin@gmail.com>
2022-03-07 02:42:56 +0300

    add create donate hook

---
+++ client/src/hooks/useCreateDonate.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError } from 'axios';
+import { CreateDonateDto } from 'dto/CreateDonateDto';
+import { useSnackbar } from 'notistack';
+import { useMutation } from 'react-query';
+
+export const useCreateDonate = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  return useMutation<void, AxiosError, CreateDonateDto>(
+    (createDonateDto) => axios.post('/donate', createDonateDto),
+    {
+      onError: (error) => {
+        if (error.response?.data?.message) {
+          enqueueSnackbar(error.response?.data?.message, {
+            variant: 'error',
+          });
+          return;
+        }
+        enqueueSnackbar('An error occurred while sending, please try again', {
+          variant: 'error',
+        });
+      },
+      onSuccess: () => {
+        enqueueSnackbar('Thank you for your donation!', {
+          variant: 'success',
+        });
+      },
+    },
+  );
+};
