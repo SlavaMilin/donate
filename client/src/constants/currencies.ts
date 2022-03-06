f8f37de14ba7fffe10b758917037b0a47f7022c3
SlavaMilin <slava.milin@gmail.com>
2022-03-07 02:40:05 +0300

    add client constants

---
+++ client/src/constants/currencies.ts
@@ -0,0 +1,6 @@
+export const currencies = [
+  { name: 'US Dollar', code: 'USD', symbol: '$', rate: 1 },
+  { name: 'Euro', code: 'EUR', symbol: '€', rate: 0.897597 },
+  { name: 'British Pound', code: 'GBP', symbol: '£', rate: 0.81755 },
+  { name: 'Russian Ruble', code: 'RUB', symbol: '₽', rate: 150.461993 },
+];
